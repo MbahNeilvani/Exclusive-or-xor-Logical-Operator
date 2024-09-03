c643218f04dfccb3f0853174f33a3ea7a93cb6af
Mbah Neilvani <neilvanimbah@gmail.com>
2024-09-03 20:40:39 +0100

    solving a boolen

---
+++ main.js
@@ -0,0 +1,12 @@
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+function xor(){
+    let a = 15
+    let b = 20
+    if (a && b % 5){
+        return true 
+    }else{
+        return false
+    }
+}
+
